373b558c9b7ef0f57f6e245c6f7ea2cfb415b2a8
sureshbabuna <sureshbabuna@gmail.com>
2021-12-28 12:39:08 -0500

    2 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -23,7 +23,8 @@ export class CartService {
   }
 
   getShippingPrices() {
-    return this.http.get<{ type: string; price: Number }[]>
-      ('/assets/shipping.json');    
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
   }
 }
